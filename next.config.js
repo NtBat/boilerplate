f23da2cfde00997e4bc0da62388220ea082d5817
NtBat <na.than.fjv@gmail.com>
2023-01-05 12:10:51 -0300

    created boilerplate

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+const runtimeCaching = require('next-pwa/cache')
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  disable: !isProd,
+  skipWaiting: true,
+  runtimeCaching
+})
+
+const nextConfig = withPWA({
+  // next config
+})
 module.exports = nextConfig
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,21 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import GlobalStyles from 'styles/global'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <title>React - Boilerplate</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <meta name="description" content="A Boilerplate NextJS" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
 }
